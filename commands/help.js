febca4f7230835e01ff1d20535c15f410a16b598
Marco Giannini <marco.giannini@6emme.it>
2020-09-22 17:21:36 +0200

    finished help command

--- commands/help.js
+++ commands/help.js
@@ -13,18 +13,9 @@ module.exports = {
 
         if (!args.length) {
             data.push('Here\'s a list of all my commands:');
-            data.push(commands.map(command => `\`${command.name}\``).join('\n'));
-            data.push(`\nYou can send \`${CMDPREFIX}help [command name]\` to get info on a specific command!`);
+            data.push(commands.map(command => `\`${command.name}\` ${command.usage} ${command.description}`).join('\n'));
 
-            return message.channel.send(data, { split: true })
-                .then(() => {
-                    if (message.channel.type === 'dm') return;
-                    message.reply('I\'ve sent you a DM with all my commands!');
-                })
-                .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-                });
+            return message.channel.send(data)
         }
     }
 };
